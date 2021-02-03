19412be0a0fe690af352ca97eee0e6e1e8966ee7
Esa <ezcoo99@gmail.com>
2021-02-03 12:48:53 +0200

    Add exercise 1.3

--- osa1/osa1.1-1.2/kurssitiedot/src/index.js
+++ osa1/osa1.1-1.2/kurssitiedot/src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1Name={part1} part1Exercises={exercises1} part2Name={part2} part2Exercises={exercises2} part3Name={part3} part3Exercises={exercises3} />
-      <Total totalCredits={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total totalCredits={[part1, part2, part3]} />
     </div>
   )
 }
@@ -30,7 +36,7 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part partName={props.part1Name} partExercises={props.part1Exercises} />
+      <Part partName={props.name} partExercises={props.exercises} />
       <Part partName={props.part2Name} partExercises={props.part2Exercises} />
       <Part partName={props.part3Name} partExercises={props.part3Exercises} />
     </div>
@@ -46,6 +52,11 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  const exercisesArray = Array.from(props) 
+  const totalExercises = props.forEach(value => {
+    console.log(value)
+  })
+
   return (
     <div>
       <p>Number of exercises: {props.totalCredits}</p>
