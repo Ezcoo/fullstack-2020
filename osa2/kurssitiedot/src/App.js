6dbd57b819cc23ce50f9883326adb523c68581ef
Esa Oksman <ezcoo99@gmail.com>
2021-05-09 00:16:48 +0300

    Fix 2.4 (kurssitiedot step9) + add 2.5

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Course from './Course'
 
 const App = () => {
     const courses =  
@@ -44,61 +45,10 @@ const App = () => {
   
     return (
       <div>
-        {courses.map(course => <Course course={course} />) }
+        {courses.map(course => <Course key={course.id} course={course} />) }
       </div>
     )
   }
 
-  const Course = (props) => {
-    return (
-      <div>
-        <Header header={props.course} />
-        <Content content={props.course} />
-        <Total course={props.course} />
-      </div>
-    )
-  }
   
-  const Header = (props) => {
-    return (
-      <div>
-        <h1>{props.header.name}</h1>
-      </div>
-    )
-  }
-  
-  const Content = (props) => {
-    return (
-      <div>
-        {props.content.parts.map(part => 
-        <Part key={part.id} part={part} />
-        )}
-      </div>
-    )
-  }
-  
-  const Part = (props) => {
-    return (
-      <div>
-        <p>{props.part.name} {props.part.exercises}</p>
-      </div>
-    )
-  }
-
-  const Total = (props) => {
-
-    const parts = props.course.parts
-    console.log(parts)
-
-    const total = parts
-                  .reduce((sum, part) => sum + part.exercises, 0)
-
-    return (
-      <div>
-        <h3>total of {total} exercises</h3>
-      </div>
-    )
-  }
-
-
   export default App
